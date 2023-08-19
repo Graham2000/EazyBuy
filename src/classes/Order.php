076e1b0064b815b67d5c3acd7c8a1f58e09deccb
William Hilton <wghiltonreads@gmail.com>
2023-08-19 00:53:01 -0400

    Add set methods to Order class

---
+++ src/classes/Order.php
@@ -0,0 +1,35 @@
+<?php 
+class Order 
+{
+    private int $orderID;
+
+    // Create new order for user
+    public function setOrder($userID)
+    {
+        $sql = "INSERT INTO user_order (user_id)
+                VALUES (?)";
+        $pdo = $this->openConn();
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$userID]);
+        $this->setOrderID($pdo->lastInsertId());
+    }
+
+    // Add product to user's order
+    public function setProduct($productID, $quantity, $orderID)
+    {
+        $sql = "INSERT INTO order_product (product_id, quantity, user_order_id)
+                VALUES (?, ?, ?)";
+        $stmt = $this->openConn()->prepare($sql);
+        $stmt->execute([$productID, $quantity, $orderID]);
+    }
+
+    public function setOrderID($orderID)
+    {
+        $this->orderID = $orderID;
+    }
+
+    public function getOrderID()
+    {
+        return $this->orderID;
+    }
+}
